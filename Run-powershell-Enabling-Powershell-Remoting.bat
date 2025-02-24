65b05ae7a775b6b311c8750138dfb3d5c0ba16bf
ranandraj <ranandraj@gmail.com>
2025-02-24 08:20:21 +0530

    Delete Run-powershell-Enabling-Powershell-Remoting.bat

--- Run-powershell-Enabling-Powershell-Remoting.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Set the PowerShell execution policy to Bypass temporarily and run the PowerShell script
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Enabling-Powershell-Remoting.ps1"
-
-:: Pause for any output (optional)
-pause
